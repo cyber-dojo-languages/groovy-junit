aff808a5d630ca18107c10d1c06897178502ea84
Jon Jagger <jon@jaggersoft.com>
2017-09-15 18:55:09 +0100

    delete dead [rm *.class] now runner is stateless

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,5 +1,8 @@
-rm -f *.class
+
 groovyc *.groovy
 if [ $? -eq 0 ]; then
-  java -cp .:$(ls /groovy/*.jar | xargs | sed -e 's/ /:/g') org.junit.runner.JUnitCore `ls -1 *Test*.class | sed 's/\(.*\)\..*/\1/'`
+  # run test classes even if they are inner classes
+  java -cp .:$(ls /groovy/*.jar | xargs | sed -e 's/ /:/g') \
+    org.junit.runner.JUnitCore \
+      `ls -1 *Test*.class | sed 's/\(.*\)\..*/\1/'`
 fi
